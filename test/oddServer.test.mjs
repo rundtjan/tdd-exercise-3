1c09870bd9848ad6b25e9d8b1340af601df62881
Jan Rundt <jan.rundt@gmail.com>
2022-05-02 18:18:44 +0300

    checking randomness and assigning port with 0

--- test/oddServer.test.mjs
+++ test/oddServer.test.mjs
@@ -2,7 +2,15 @@ import { expect } from "chai";
 import { start } from "../src/oddServer.mjs";
 
 describe("Test without refactor", () => {
-  it("First test", () => {
-    expect(start('name', 8000)).to.be.an('object');
+  it("Returns an object", () => {
+    console.log(start('name', 0))
+    expect(start('name', 0)).to.be.an('object');
+  });
+
+  it("Returns random contents", () => {
+    let contents = [];
+    let i = 0;
+    while (true) {contents.push(start('name', 0).content); i++; if (i === 100) break;}
+    expect(contents.filter(elem => elem === contents[0]).length).to.be.below(100);
   });
 });
